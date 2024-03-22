52e8bf4bfdef3a046deb9153c66d591d1697ba6d
Vlad Beltran <puentes.studio@gmail.com>
2024-03-22 14:19:15 +0100

    cats data

---
+++ src/assets/cats/catData.js
@@ -0,0 +1,136 @@
+import cat1 from "../../assets/cats/1wsg.png";
+import cat2 from "../../assets/cats/1wtb.png";
+import cat3 from "../../assets/cats/2bsr.png";
+import cat4 from "../../assets/cats/2ttg.png";
+import cat5 from "../../assets/cats/2wrb.png";
+import cat6 from "../../assets/cats/2wsr.png";
+import cat7 from "../../assets/cats/3brg.png";
+import cat8 from "../../assets/cats/3bsg.png";
+import cat9 from "../../assets/cats/3btr.png";
+import cat10 from "../../assets/cats/3trb.png";
+import cat11 from "../../assets/cats/3tsr.png";
+import cat12 from "../../assets/cats/3wrr.png";
+
+// Import images for each cat
+
+const catData = [
+  {
+    id: "1",
+    imageUrl: cat1,
+    stripes: 1,
+    color: "w",
+    shape: "s",
+    eyes: "g",
+    description:
+      "This cat has 1 stripe, is white, is tall, and has green eyes.",
+  },
+  {
+    id: "2",
+    imageUrl: cat2,
+    stripes: 1,
+    color: "w",
+    shape: "t",
+    eyes: "b",
+    description: "This cat has 1 stripe, is white, is tall, and has blue eyes.",
+  },
+  {
+    id: "3",
+    imageUrl: cat3,
+    stripes: 2,
+    color: "b",
+    shape: "s",
+    eyes: "r",
+    description:
+      "This cat has 2 stripes, is black, is short, and has red eyes.",
+  },
+  {
+    id: "4",
+    imageUrl: cat4,
+    stripes: 2,
+    color: "t",
+    shape: "t",
+    eyes: "g",
+    description:
+      "This cat has 2 stripes, is gray, is tall, and has green eyes.",
+  },
+  {
+    id: "5",
+    imageUrl: cat5,
+    stripes: 2,
+    color: "w",
+    shape: "r",
+    eyes: "b",
+    description:
+      "This cat has 2 stripes, is white, is rounded, and has blue eyes.",
+  },
+  {
+    id: "6",
+    imageUrl: cat6,
+    stripes: 2,
+    color: "w",
+    shape: "s",
+    eyes: "r",
+    description:
+      "This cat has 2 stripes, is white, is short, and has red eyes.",
+  },
+  {
+    id: "7",
+    imageUrl: cat7,
+    stripes: 3,
+    color: "b",
+    shape: "r",
+    eyes: "g",
+    description:
+      "This cat has 3 stripes, is black, is rounded, and has green eyes.",
+  },
+  {
+    id: "8",
+    imageUrl: cat8,
+    stripes: 3,
+    color: "b",
+    shape: "s",
+    eyes: "g",
+    description:
+      "This cat has 3 stripes, is black, is short, and has green eyes.",
+  },
+  {
+    id: "9",
+    imageUrl: cat9,
+    stripes: 3,
+    color: "b",
+    shape: "t",
+    eyes: "r",
+    description: "This cat has 3 stripes, is black, is tall, and has red eyes.",
+  },
+  {
+    id: "10",
+    imageUrl: cat10,
+    stripes: 3,
+    color: "t",
+    shape: "r",
+    eyes: "b",
+    description:
+      "This cat has 3 stripes, is gray, is rounded, and has blue eyes.",
+  },
+  {
+    id: "11",
+    imageUrl: cat11,
+    stripes: 3,
+    color: "t",
+    shape: "s",
+    eyes: "r",
+    description: "This cat has 3 stripes, is gray, is short, and has red eyes.",
+  },
+  {
+    id: "12",
+    imageUrl: cat12,
+    stripes: 3,
+    color: "w",
+    shape: "r",
+    eyes: "r",
+    description:
+      "This cat has 3 stripes, is white, is rounded, and has red eyes.",
+  },
+];
+
+export default catData;
